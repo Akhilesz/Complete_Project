b0854a2400c3fe00e38b7863f71a4fb8e29d5b4f
thokali-a <thokali.a@flipkart.com>
2025-10-04 08:57:24 +0530

    first commit

--- frontend/reactjs/src/App.js
+++ frontend/reactjs/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hi Akhileshwar... welcome to React world!
         </a>
       </header>
     </div>
