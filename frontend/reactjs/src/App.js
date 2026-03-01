d5aa178404bdc1e5fbd44484ed2bff107bfe1f4e
thokali-a <thokali.a@flipkart.com>
2026-03-01 11:10:01 +0530

    first commit

--- frontend/reactjs/src/App.js
+++ frontend/reactjs/src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
+import react from 'react';
 import './App.css';
 
 function App() {
+  const [todos , setTodos] = react.useState([]);
+  const [newTodo , setNewTodo] = react.useState('');
+
+
+  addNewTodo = () => {
+      
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hi Akhileshwar... welcome to React world!
-        </a>
-      </header>
+      <h1>
+        Todo List
+      </h1>
     </div>
   );
 }
